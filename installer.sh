7b319c11e9af6034a637134e28b11f0a5b748401
Mike Wallio <michael.wallio@outlook.com>
2020-11-30 19:19:37 -0500

    Merge branch 'uwp-apps' of https://github.com/scaryrawr/winapps into uwp

--- installer.sh
+++ installer.sh
@@ -38,9 +38,14 @@ function waFindInstalled() {
 		cp "${DIR}/install/ExtractPrograms.ps1" ${HOME}/.local/share/winapps/ExtractPrograms.ps1
 		for F in $(ls "${DIR}/apps"); do
 			if [[ -d "${DIR}/apps/${F}" ]]; then
-			    . "${DIR}/apps/${F}/info"
-			    [[ ${F} != "windows" ]] && echo "IF EXIST \"${WIN_EXECUTABLE}\" ECHO ${F} >> \\\\tsclient\\home\\.local\\share\\winapps\\installed.tmp" >> ${HOME}/.local/share/winapps/installed.bat
-			fi
+                            . "${DIR}/apps/${F}/info"
+                            if [ -z "${UWP_NAME}" ]; then
+                                    [[ ${F} != "windows" ]] && echo "IF EXIST \"${WIN_EXECUTABLE}\" ECHO ${F} >> \\\\tsclient\\home\\.local\\share\\winapps\\installed.tmp" >> ${HOME}/.local/share/winapps/installed.bat
+                            else
+                                    echo "powershell.exe if (Get-AppxPackage -Name ${UWP_NAME}) { echo ${F} >> \\\\tsclient\\home\\.local\\share\\winapps\\installed.tmp }" >> ${HOME}/.local/share/winapps/installed.bat
+                                    UWP_NAME=''
+                            fi
+                        fi
 		done;
 		echo "powershell.exe -ExecutionPolicy Bypass -File \\\\tsclient\\home\\.local\\share\\winapps\\ExtractPrograms.ps1 > \\\\tsclient\home\\.local\\share\\winapps\\detected" >> ${HOME}/.local/share/winapps/installed.bat
 		echo "RENAME \\\\tsclient\\home\\.local\\share\\winapps\\installed.tmp installed" >> ${HOME}/.local/share/winapps/installed.bat
